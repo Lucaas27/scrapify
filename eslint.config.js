8396ad79f848c9f2fedc1dcce40648cbb1ebcb9c
Lucas Gomes <lucasggomees@gmail.com>
2025-03-21 18:23:45 +0000

    feat: initialize project with Vite, React, and TypeScript setup; add ESLint, Prettier, and commitlint configurations

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from "eslint/config"
+import globals from "globals"
+import js from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  { ignores: [".node_modules/*", "dist", "**.config.*"] },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "react/react-in-jsx-scope": "off",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+      "no-unused-expressions": [
+        "error",
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+          allowTaggedTemplates: true,
+        },
+      ],
+    },
+  },
+])
