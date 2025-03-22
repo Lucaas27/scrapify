79cdf5fb2838c85865144199a1b18a9a004d6881
Lucas Gomes <lucasggomees@gmail.com>
2025-03-22 00:24:02 +0000

    chore: update dummy sum function with return type and improve tests; update dependencies

--- src/__tests__/sum.spec.ts
+++ src/__tests__/sum.spec.ts
@@ -1,8 +1,9 @@
-import { sum } from "#src/utils/sum.ts"
 import { describe, expect, it } from "vitest"
-
+import { sum } from "#src/utils/sum.ts"
 describe("sum function", () => {
-  it("should add two positive numbers correctly", () => {
-    expect(sum(1, 2)).toBe(3)
+  it("adds two numbers correctly", () => {
+    expect(sum(1, 2)).toEqual(3)
+    expect(sum(5, 7)).toEqual(12)
+    expect(sum(-1, 1)).toEqual(0)
   })
 })
